afe21a9181e2af0e12bb255a1453524703c42be5
guz <gabriel@guz.gz>
2024-11-11 15:50:15 +0000

    chore: lint

--- src/utils/verifyPassword.util.ts
+++ src/utils/verifyPassword.util.ts
@@ -1,6 +1,8 @@
-
 import * as argon2 from 'argon2';
 
-export const verifyPassword = async (hashedPassword: string, password: string) => {
-  return await argon2.verify(hashedPassword, password)
+export const verifyPassword = async (
+  hashedPassword: string,
+  password: string
+) => {
+  return await argon2.verify(hashedPassword, password);
 };
